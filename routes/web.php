434363644a9abbcc01854cf0b81546fad31cf14b
JAIMIN-DESTINY <jaimin.dhaduk@thedestinysolutions.com>
2026-06-29 11:00:29 +0530

    added chnage

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\MobilesentrixController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('date-check', function () {
+    $date = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+    return $date;
+})->name('date-check');
 Route::get('/', [MobilesentrixController::class, 'Dashboard'])->name('ms-dashboard');
 
 Route::get('ms-category', [MobilesentrixController::class, 'MsCategory'])->name('ms-category');
