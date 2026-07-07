c4ab0a5c41c371cd26c98ed4aa4762d0cd88af9e
JAIMIN-DESTINY <jaimin.dhaduk@thedestinysolutions.com>
2026-07-07 12:00:18 +0530

    added chnage

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('date-check', function () {
     return $date;
 })->name('date-check');
 
+// Mobilesentrix
 Route::get('ms-category', [MobilesentrixController::class, 'MsCategory'])->name('ms-category');
 Route::get('ms-product', [MobilesentrixController::class, 'MsProduct'])->name('ms-product');
 Route::get('ms-product-sync', [MobilesentrixController::class, 'MsProductSync'])->name('ms-product-sync');
@@ -16,9 +17,10 @@ Route::get('ms-product-sync', [MobilesentrixController::class, 'MsProductSync'])
 Route::get('/', [MobilesentrixController::class, 'Dashboard'])->name('ms-dashboard');
 Route::get('ms-products/export', [MobilesentrixController::class, 'ExportAllProducts'])->name('ms-products.export');
 
-Route::get('p4c-dashboard', [Parts4CellsController::class, 'Dashboard'])->name('p4c-dashboard');
-Route::get('p4c-products/export', [Parts4CellsController::class, 'ExportAllProducts'])->name('p4c-products.export');
-
+// Parts4Cells
 Route::get('p4c-category', [Parts4CellsController::class, 'P4cCategory'])->name('p4c-category');
 Route::get('p4c-product', [Parts4CellsController::class, 'P4cProduct'])->name('p4c-product');
 Route::get('p4c-product-sync', [Parts4CellsController::class, 'P4cProductSync'])->name('p4c-product-sync');
+
+Route::get('p4c-dashboard', [Parts4CellsController::class, 'Dashboard'])->name('p4c-dashboard');
+Route::get('p4c-products/export', [Parts4CellsController::class, 'ExportAllProducts'])->name('p4c-products.export');
